c0df2946138b6e1d275e1cc77729679e4efb7a5e
carolinenpark2 <168035005+carolinenpark2@users.noreply.github.com>
2025-04-06 10:39:21 -0700

    Update association_rule.py
    fpg association rule

--- association_rule.py
+++ association_rule.py
@@ -21,8 +21,21 @@ df = pd.DataFrame(te_array, columns = te.columns_)
 # drop missing values
 df = df.drop('none', axis = 1)
 
-# show frequent itemsets that appear together in at least 20% of transactions
+# show frequent itemsets that appear together in at least 20% of transactions using apriori
 freq_itemsets = apriori(df, min_support = 0.2, use_colnames = True)
 
 # generate association rules from the frequent itemsets
 res = association_rules(freq_itemsets, metric = 'confidence', min_threshold = 0.4)
+
+# subset dataframe
+res1 = res[['antecedents', 'consequents', 'support', 'confidence', 'lift']]
+
+# sort values based in lift
+sorted_res1 = res1.sort_values(by = 'lift')
+
+# show frequent itemsets that appear together in at least 20% of transactions using fpg
+freq_itemsets = fpgrowth(df, min_support = 0.2, use_colnames = True)
+
+resfpg = association_rules(freq_itemsets, metric = 'confidence', min_threshold = 0.4)
+
+
